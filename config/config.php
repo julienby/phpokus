fa4010bd828737fa67c8cd2ce219264740993554
Julien BY <156668471+julienby@users.noreply.github.com>
2024-08-18 17:40:09 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,11 +2,11 @@
 
 // Définition de la constante BASE_URL
 if (!defined('BASE_URL')) {
-    define('BASE_URL', 'https://seinebnb.com/');
+    define('BASE_URL', 'https://test.com/');
 }
 // Retour de la configuration de la base de données
 return [
     'database' => [
-        'dsn' => 'sqlite:' . __DIR__ . '/../database/seinebnb.db',
+        'dsn' => 'sqlite:' . __DIR__ . '/../database/test.db',
     ],
 ];
